e2ee5617427a80f3f57b26a9322ec60d0fa47502
a-saitou <a-saitou@sirius.ocn.ne.jp>
2016-08-15 21:25:16 +0900

    (

--- app/controllers/agreement_controller.rb
+++ app/controllers/agreement_controller.rb
@@ -2,7 +2,7 @@ class AgreementController < ApplicationController
   def index
   	@agreements_grid = initialize_grid(Agreement, 
   		include: :orderer,
-      order:    'orderers.name',
+      order:    'agreements.name',
       per_page: 20)
   end
   # GET /agreements/1
